897d4c3dec22e6ff7a607ae0b682949a498f055a
Pratyush <95492798+PR110895@users.noreply.github.com>
2022-04-20 23:14:18 +0530

    4th commit added cart,checkou component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./componenet/Home";
 import { Switch, Route } from "react-router-dom";
 import Products from "./componenet/Products";
 import Product from "./componenet/Product";
+import Cart from "./componenet/Cart";
+import Checkout from "./componenet/Checkout";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/products" component={Products} />
         <Route exact path="/products/:id" component={Product} />
+        <Route exact path="/cart" component={Cart} />
+        <Route exact path="/checkout" component={Checkout} />
       </Switch>
     </div>
   );
